7c5e02e8f171ef77cee95dc130ae91e3c74ea2cd
kcprater <kristopherprater@gmail.com>
2020-08-18 10:00:59 -0400

    Add project files.

---
+++ GeneralStore MVC/Controllers/ProductController.cs
@@ -0,0 +1,67 @@
+﻿using GeneralStore_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeneralStore_MVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        // Add the application DB Context (link to the database)
+        private ApplicationDbContext _db = new ApplicationDbContext();
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View(_db.Products.ToList());
+        }
+
+        //GET: Product/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+        //POST: Product/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken] //may not need????????????????????????
+        public ActionResult Create (Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Products.Add(product);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+        //GET: Delete a Product
+        //Product/Delete/{id}
+        public ActionResult Delete (int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = _db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+        //POST: Delete Product
+        //Product/Delete/{id}
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            Product product = _db.Products.Find(id);
+            _db.Products.Remove(product);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
